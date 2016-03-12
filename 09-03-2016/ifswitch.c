ed520b1f1f6f6ad826cfcd40215d3ac4f56fdc0c
Bartosz Kostaniak <bartosz@localhost.localdomain>
2016-03-12 22:55:50 +0100

    Program włączający/wyłączający podany interfejs sieciowy

---
+++ 09-03-2016/ifswitch.c
@@ -0,0 +1,46 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    int sfd;
+    struct ifreq ifr;
+    char *on_or_off = (char*)malloc(100);
+
+    strncpy(on_or_off, argv[2], 100);
+
+    // utwórz socket dla funkcji ioctl i przypisz nazwę interfejsu na podstawie pierwszego argumentu
+    sfd = socket(PF_INET, SOCK_DGRAM, 0);
+    strncpy(ifr.ifr_name, argv[1], IFNAMSIZ);
+    
+    // pobierz flagi interfejsu sieciowego
+    ioctl(sfd, SIOCGIFFLAGS, &ifr);
+    // wykonaj odpowiednią akcję na podstawie drugiego argumentu
+    if (strcmp(on_or_off, "up") == 0) {
+        ifr.ifr_flags |= IFF_UP;
+    } else if (strcmp(on_or_off, "down") == 0) {
+        ifr.ifr_flags &= ~IFF_UP;
+    } else {
+        // wyświetl komunikat o błędzie
+        printf("Invalid option: %s\nUse 'up' or 'down' instead\n", on_or_off);
+        free(on_or_off);
+        exit(1);
+    }
+
+    // ustaw odpowiednie flagi (włączenie lub wyłączenie interfejsu)
+    ioctl(sfd, SIOCSIFFLAGS, &ifr);
+
+    // sprzątanie
+    free(on_or_off);
+}
